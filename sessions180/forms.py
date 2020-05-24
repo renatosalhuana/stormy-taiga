66c6f4fb74671a87e33ad7c3578baaeab3cc4e04
Renato Salhuana <rsalhuana@gmail.com>
2020-05-24 12:42:55 -0500

    image tarea dia 5

--- sessions180/forms.py
+++ sessions180/forms.py
@@ -4,9 +4,10 @@ from django.utils.translation import gettext_lazy as _
 
 
 class StudentPresentationForm(forms.ModelForm):
+    
     class Meta:
         model = Student
         fields = ("name",)
         labels = {
-            'name': _('Cuéntame, ¿cuál es tu nombre?')
+            'name': _('¿cuál es tu nombre?')
         }
